d770d1ba8d6f450b697ae008337450cc52713134
Shubham Bhardwaj <shubham200015@yahoo.com>
2022-04-19 20:09:16 +0530

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './Components/Home/Home'
 import Weather from './Components/Weather/Weather'
 import News from './Components/News/News'
+import Navbar from './Components/Navbar/Navbar'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route exact path="/weather" element={<Weather />} />

632f6a4d959f5a0893eb7709bf6740f0abf52290
Shubham Bhardwaj <shubham200015@yahoo.com>
2022-05-27 13:21:06 +0530

    Add responsivity to navabr, h1 and map

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../Images/Logo.svg'
+import Hamburger from '../../Images/Hamburger_icon.svg.png'
 
 // Styles
 import './Navbar.css'
 
 export default function Navbar() {
+    const [open, setOpen] = useState(false);
+
+    const toggleOpen = () => {
+        setOpen((openState) => !openState);
+    }
     return (
         <div className='Navbar'>
             <div className='Navbar-container'>
@@ -18,7 +24,20 @@ export default function Navbar() {
                     <Link to='/weather'>Weather</Link>
                     <Link to='/news'>News</Link>
                 </div>
+                <div className='Navbar-hamburger' onClick={toggleOpen}>
+                    <img src={Hamburger} alt="Hamburger-icon" style={{ width: "40px" }} />
+                </div>
             </div>
+            {
+                open &&
+                <div className='Navbar-dropdown'>
+                    <div className='Navbar-dropdown-links'>
+                        <Link to='/'>Home</Link>
+                        <Link to='/weather'>Weather</Link>
+                        <Link to='/news'>News</Link>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
